07abc1a3bf0f1e410c5beee7af6128606c7c9366
Imran Shafiq <imyshafiq@hotmail.co.uk>
2015-04-10 04:50:50 +0100

    Added in sounds for main menu scene.

--- Unity Project/Assets/Scenes/Main Menu Scene/StartMenuUIScript.cs
+++ Unity Project/Assets/Scenes/Main Menu Scene/StartMenuUIScript.cs
@@ -5,8 +5,35 @@ public class StartMenuUIScript : MonoBehaviour {
 
 	public void LoadGame()
 	{
+		StartCoroutine(PlayButtonClick());
 		Application.LoadLevel("MainScene");
 		Debug.Log("Main Game Scene Is Loaded");
 	}
 
+	public void donateButton()
+	{
+		StartCoroutine(PlayButtonClick());
+	}
+	public void shareButton()
+	{
+		StartCoroutine(PlayButtonClick());
+	}
+	public void quitButton()
+	{
+		StartCoroutine(PlayButtonClick());
+		Application.Quit();
+	}
+	public void settingsButton()
+	{
+		StartCoroutine(PlayButtonClick());
+	}
+	public void trophyButton()
+	{
+		StartCoroutine(PlayButtonClick());
+	}
+	IEnumerator PlayButtonClick()
+	{
+		audio.Play();
+		yield return new WaitForSeconds(audio.clip.length);
+	}
 }
